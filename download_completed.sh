01749f817b3751228a58ebf95560191c0740adcf
James Hart <hjhart@gmail.com>
2012-12-09 21:42:19 -0800

    Error catching

--- download_completed.sh
+++ download_completed.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env ruby
+begin
+  require 'rubygems'
+  require 'prowly'
+  require 'logger'
 
-require 'rubygems'
-require 'prowly'
-require 'logger'
+  api_key = File.open('/Users/james/.prowl').read
+  api_key.strip!
 
-api_key = File.open('/Users/james/.prowl').read
-api_key.strip!
+  torrent_name = ENV['TR_TORRENT_NAME']
 
-torrent_name = ENV['TR_TORRENT_NAME']
+  file_to_log_to = File.join("/", "var", "log", "transmission", "notifier.log")
+  log = Logger.new(file_to_log_to)
 
-file_to_log_to = File.join("/", "var", "log", "transmission", "notifier.log")
-log = Logger.new(file_to_log_to)
+  log.debug "Attempting to log successful download of #{torrent_name}"  
 
-log.debug "Attempting to log successful download of #{torrent_name} with API key #{api_key}"  
+  notification = Prowly::Notification.new(:apikey => api_key, :application => "Transmission", :event => "Download completed", :description => "#{torrent_name} finished at #{Time.now}") 
+  result = Prowly.notify(notification)
 
-notification = Prowly::Notification.new(:apikey => api_key, :application => "Transmission", :event => "Download completed", :description => "#{torrent_name} finished at #{Time.now}") 
-result = Prowly.notify(notification)
+  if(result.succeeded?)
+    log.debug "notification successfully sent!"
+  else
+    log.error "notification could not be sent!"
+    log.error "#{result.message}"
+  end
 
-if(result.succeeded?)
-  log.debug "notification successfully sent!"
-else
-  log.error "notification could not be sent!"
-  log.error "#{result.message}"
+rescue Exception => exception
+  log.error "An error happened in the excecution of the screeept"
+  log.error exception.backtrace
+  raise exception
 end
