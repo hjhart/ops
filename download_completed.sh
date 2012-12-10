6eae809d56058a52d4a3b9c9b914ddcc5fbc5f06
James Hart <hjhart@gmail.com>
2012-12-09 21:24:47 -0800

    Download completed completed.

--- download_completed.sh
+++ download_completed.sh
@@ -4,13 +4,15 @@ require 'rubygems'
 require 'prowly'
 require 'logger'
 
-api_key = File.open('~/.prowl').read
+api_key = File.open('/Users/james/.prowl').read
+api_key.strip!
+
 torrent_name = ENV['TR_TORRENT_NAME']
 
-file_to_log_to = File.join("var", "log", "transmission", "notifier.log")
+file_to_log_to = File.join("/", "var", "log", "transmission", "notifier.log")
 log = Logger.new(file_to_log_to)
 
-log.debug "Attempting to log successful download of #{torrent_name}"  
+log.debug "Attempting to log successful download of #{torrent_name} with API key #{api_key}"  
 
 notification = Prowly::Notification.new(:apikey => api_key, :application => "Transmission", :description => "#{torrent_name} has finished downloading!") 
 result = Prowly.notify(notification)
